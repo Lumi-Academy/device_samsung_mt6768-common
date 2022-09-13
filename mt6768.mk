64ebffe9013c3b77b4d3d979718fae95566ec983
Alessandro Astone <ales.astone@gmail.com>
2022-09-13 18:56:16 +0100

    mt6768-common: Build fastbootd

--- mt6768.mk
+++ mt6768.mk
@@ -48,6 +48,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
