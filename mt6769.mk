511b985e70c283924c4f92475957f3615873bbbb
HoangLong-Lumi <111274514+HoangLong-Lumi@users.noreply.github.com>
2022-10-09 20:17:20 +0700

    mt6769-common: Initial device tree Signed-off-by: Nguyễn Long <hoanglong.lumi404@gmail.com>

---
+++ mt6769.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# AAPT
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
+
+# Dynamic Partition
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION := false
+
+# Extra VNDK Versions
+PRODUCT_EXTRA_VNDK_VERSIONS := 30
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
+# Inherit from vendor if exists
+$(call inherit-product-if-exists, vendor/samsung/mt6769-common/mt6769-common-vendor.mk)
