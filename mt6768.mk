6d6425f17bbd0b8493b2603b391cf8352e904811
HoangLong-Lumi <hoanglong.lumi404@gmail.com>
2022-12-11 11:31:25 +0700

    mt6768-common: Build fingerprint

--- mt6768.mk
+++ mt6768.mk
@@ -115,6 +115,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.samsung
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
