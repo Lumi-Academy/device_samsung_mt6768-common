2e2cd40569849efe12faccfc86e4ab6662584623
HoangLong-Lumi <hoanglong.lumi404@gmail.com>
2022-12-11 12:05:10 +0700

    mt6768-common: Switch sensors to AIDL

--- mt6768.mk
+++ mt6768.mk
@@ -290,7 +290,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors-service.samsung-multihal \
     android.hardware.sensors@2.0-ScopedWakelock.vendor
 
 PRODUCT_PACKAGES += \
